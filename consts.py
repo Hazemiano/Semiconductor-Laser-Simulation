2b4f002f983da4e3432c073fcba51b6a77adfaa3
AliNowia <rtkx99999@gmail.com>
2026-06-02 22:48:06 +0300

    fixed C to be vg

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 X = 4
 Y = 4
 L = (200+30*X)*1e-4
@@ -8,9 +10,19 @@ GAMMA = 0.3
 NG = 4
 ALPHA_INT = 40
 B = 2.5e-16
-N0 = 1e18
-TAU = 2.2e-9
-BETA_SP = 1e-4
+N0 = 1e18 # transparency carrier density
+TAU = 2.2e-9 # spontaneous lifetime
+BETA_SP = 1e-4 # spontaenous emission factor
 H = 6.62607015e-34
 E = 1.6e-19
 C = 3e10
+
+vg = C/NG # group velocity
+alpha_tot = ALPHA_INT + 0.5*np.log(1/(R*R))/L # total alpha loss
+ALPHA_M=(1/(2*L))*(np.log((1/R*R)))
+V = L * D * W # volume of the diode
+tau_ph = 1/(vg*alpha_tot) # photon lifetime 
+g_th = alpha_tot / GAMMA; # gain 
+q = 1.6*1e-19 # electric charge
+lamda = 1.55e-4 # operating wavelength (assumed to be 1.55um)
+freq = C/lamda
